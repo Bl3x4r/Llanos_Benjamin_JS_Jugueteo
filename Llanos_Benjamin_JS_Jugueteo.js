50a43e386dcc114c181cf513cd70ef75b606d779
Benjamin L <103331861+Bl3x4r@users.noreply.github.com>
2023-09-02 18:52:47 -0400

    Add files via upload

---
+++ Llanos_Benjamin_JS_Jugueteo.js
@@ -0,0 +1,10 @@
+var alturaNiño = 87;
+
+function puedeSubir(){
+    if(alturaNiño > 52 ){
+        console.log("!Subete Chico¡");
+    }else{
+        console.log("Lo siento,chico.Tal vez el proximo año.");
+    }
+}
+puedeSubir(alturaNiño);
